cc5ded6d966bd6e29d01ec46869bc0805a82262e
Nahid Hasan Noyon <nahidhasannoyon20@gmail.com>
2025-03-25 15:35:16 +0600

    feat: Enhance app configuration with fallback for API_URL; add debug prints for environment mode

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,11 +1,20 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:flutter_repo_hunter/domain/enums/app_mode.dart';
+
+import '../../domain/enums/app_mode.dart';
 
 class AppConfig {
   static const String appName = 'Flutter App';
   static const String slogan = 'Your Slogan Here';
   static const String appVersion = '1.0.0';
 
-  static String baseUrl = dotenv.env['API_URL']!;
+  // Provide a fallback value if the API_URL is not found
+  static String get baseUrl {
+    final url = dotenv.env['API_URL'];
+    if (url == null) {
+      throw Exception("API_URL is not defined in the environment file.");
+    }
+    return url;
+  }
+
   static String appMode = AppMode.development.toString();
 }
--- lib/core/config/app_initializer.dart
+++ lib/core/config/app_initializer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_repo_hunter/core/config/app_config.dart';
 import 'package:get_storage/get_storage.dart';
@@ -8,9 +9,16 @@ import '../helpers/translation_helper.dart';
 Future<void> initializeApp() async {
   if (AppConfig.appMode == AppMode.development.toString()) {
     await dotenv.load(fileName: ".env.dev"); // Loads the .env file
+    if (kDebugMode) {
+      print("App is running in development mode");
+    }
   } else {
     await dotenv.load(fileName: ".env.prod"); // Loads the .env file
+    if (kDebugMode) {
+      print("App is running in production mode");
+    }
   }
+
   await GetStorage.init(); // Initialize GetStorage
   await TranslationHelper.initialize(); // Initialize translations
 }
